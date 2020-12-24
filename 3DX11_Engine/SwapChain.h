2313cff53a4fa2e25a14073b9a37d39a9b056329
Diope <dionp@fullsail.edu>
2020-12-24 18:30:23 -0500

    First iteration, committing to fix a potential issue in the swap chain branch

--- 3DX11_Engine/SwapChain.h
+++ 3DX11_Engine/SwapChain.h
@@ -1,17 +1,25 @@
 #pragma once
 #include <d3d11.h>
 
+class DeviceContext;
 class SwapChain
+
 {
 public:
 	SwapChain();
 	// Initialize Swapchain for a window
 	bool init(HWND hwmd, UINT width, UINT height);
 
+	bool present(bool vsync);
+
 	// Release the Swapchain
 	bool release();
 	~SwapChain();
 private:
 	IDXGISwapChain *m_swap_chain;
+	ID3D11RenderTargetView *m_rtv;
+
+private:
+	friend class DeviceContext;
 };
 

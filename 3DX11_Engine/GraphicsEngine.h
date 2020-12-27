34ac5c4667635bbfcbfae80f2ff740c6f433acc6
Diope <dionp@fullsail.edu>
2020-12-26 20:22:14 -0500

    Added the compileVertexShader method and functionality, working on releaseCompiledShader functionality next

--- 3DX11_Engine/GraphicsEngine.h
+++ 3DX11_Engine/GraphicsEngine.h
@@ -5,6 +5,7 @@
 class SwapChain;
 class DeviceContext;
 class VertexBuffer;
+class VertexShader;
 
 class GraphicsEngine
 {
@@ -18,11 +19,19 @@ public:
 	~GraphicsEngine();
 
 public:
+
+
 	SwapChain *createSwapChain();
 	DeviceContext *getImmediateDeviceContext();
 	VertexBuffer *createVertexBuffer();
+	VertexShader *createVertexShader(const void *shader_byte_code, size_t byte_code_size);
 
 public:
+
+	bool compileVertexShader(const wchar_t *file_name, const char *entry_point_name, void** shader_byte_code, size_t *byte_code_size);
+	void releaseCompiledShader();
+
+	// Default simple shaders
 	bool createShaders();
 	bool setShaders();
 	void getShaderBufferAndSize(void **bytecode, UINT*size);
@@ -42,6 +51,9 @@ private:
 	ID3D11DeviceContext* m_imm_context;
 
 private:
+	ID3DBlob* m_blob = nullptr;
+
+
 	ID3DBlob* m_vsblob = nullptr;
 	ID3DBlob* m_psblob = nullptr;
 	ID3D11VertexShader* m_vs = nullptr;
@@ -49,5 +61,6 @@ private:
 private:
 	friend class SwapChain;
 	friend class VertexBuffer;
+	friend class VertexShader;
 };
 
---
+++ 3DX11_Engine/VertexShader.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class GraphicsEngine;
+
+class VertexShader
+{
+public:
+	VertexShader();
+
+	bool release();
+	~VertexShader();
+
+private:
+	bool init(const void *shader_byte_code, size_t byte_code_size);
+private:
+	ID3D11VertexShader *m_vs;
+private:
+	friend class GraphicsEngine;
+};
+

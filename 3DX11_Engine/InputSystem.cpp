3929cbd038be042b13b5cf3a704ef25bf6b25af1
Diope <dionp@fullsail.edu>
2020-12-30 14:32:51 -0500

    Got the input implementation from StackOverflow link. Implemented the main components of the InputListener

--- 3DX11_Engine/InputSystem.cpp
+++ 3DX11_Engine/InputSystem.cpp
@@ -1,4 +1,5 @@
 #include "InputSystem.h"
+#include <Windows.h>
 
 
 
@@ -9,4 +10,63 @@ InputSystem::InputSystem()
 
 InputSystem::~InputSystem()
 {
+}
+
+void InputSystem::update()
+{
+	if (::GetKeyboardState(m_keys_state))
+	{
+		for (unsigned int i = 0; i < 256; i++)
+		{
+			// Key is down
+			if (m_keys_state[i] & 0x80)
+			{
+				std::map<InputListener*, InputListener*>::iterator it = m_map_listeners.begin();
+
+				while (it != m_map_listeners.end())
+				{
+					it->second->onKeyDown(i);
+					++it;
+				}
+			}
+			// Key is up
+			else
+			{
+				if (m_keys_state[i] != m_old_keys_state[i])
+				{
+					std::map<InputListener*, InputListener*>::iterator it = m_map_listeners.begin();
+
+					while (it != m_map_listeners.end())
+					{
+						it->second->onKeyUp(i);
+						++it;
+					}
+				}
+			}
+		}
+		// store current keys state to old keys state buffer
+		::memcpy(m_old_keys_state, m_keys_state, sizeof(unsigned char) * 256);
+	}
+}
+
+void InputSystem::addListener(InputListener * listener)
+{
+	m_map_listeners.insert(std::make_pair<InputListener*, InputListener* >
+		(std::forward<InputListener *>(listener), std::forward<InputListener *>(listener)));
+}
+
+void InputSystem::removeListener(InputListener * listener)
+{
+	std::map<InputListener*, InputListener*>::iterator it = m_map_listeners.find(listener);
+
+	if (it != m_map_listeners.end())
+	{
+		m_map_listeners.erase(it);
+	}
+}
+
+InputSystem * InputSystem::get()
+{
+	static InputSystem system;
+	return &system;
 }

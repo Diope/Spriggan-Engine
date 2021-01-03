5ec7acbfcf6b364507aa9e022ddc4bca8ae2cf39
Diope <dionp@fullsail.edu>
2021-01-03 15:46:42 -0500

    Switched out old pointers to use C++11's smart pointers and referencing for better memory usage and not having to on manually deletion

--- 3DX11_Engine/RenderSystem.h
+++ 3DX11_Engine/RenderSystem.h
@@ -15,13 +15,13 @@ public:
 
 public:
 
-	SwapChain *createSwapChain(HWND hwmd, UINT width, UINT height);
-	DeviceContext *getImmediateDeviceContext();
-	VertexBuffer *createVertexBuffer(void *list_vertices, UINT size_vertex, UINT size_list, void *shader_byte_code, UINT size_byte_shader);
-	ConstantBuffer *createConstantBuffer(void* buffer, UINT size_buffer);
-	IndexBuffer *createIndexBuffer(void *list_indices, UINT size_list);
-	VertexShader *createVertexShader(const void *shader_byte_code, size_t byte_code_size);
-	PixelShader *createPixelShader(const void *shader_byte_code, size_t byte_code_size);
+	SwapChainPtr createSwapChain(HWND hwmd, UINT width, UINT height);
+	DeviceContextPtr getImmediateDeviceContext();
+	VertexBufferPtr createVertexBuffer(void *list_vertices, UINT size_vertex, UINT size_list, void *shader_byte_code, UINT size_byte_shader);
+	ConstantBufferPtr createConstantBuffer(void* buffer, UINT size_buffer);
+	IndexBufferPtr createIndexBuffer(void *list_indices, UINT size_list);
+	VertexShaderPtr createVertexShader(const void *shader_byte_code, size_t byte_code_size);
+	PixelShaderPtr createPixelShader(const void *shader_byte_code, size_t byte_code_size);
 
 public:
 
@@ -31,7 +31,7 @@ public:
 	void releaseCompiledShader();
 
 private:
-	DeviceContext * m_imm_device_context;
+	DeviceContextPtr m_imm_device_context;
 private:
 	ID3D11Device * m_d3d_device;
 	D3D_FEATURE_LEVEL m_feature_level;
